01de9c1db2e2a159a8cf1e05808aedeec52c0272
Niamh Browne <niamhb@alchemytechs.com>
2024-10-14 09:24:06 +0100

    Made Title, Date, Time, Location Requried for Events

--- FinalProject.Data/Entities/Event.cs
+++ FinalProject.Data/Entities/Event.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace FinalProject.Data.Entities;
 
 public class Event
 {
     public int Id { get; set; }
+    [Required]
     public string Title { get; set; }
+    [Required]
     public DateTime Date { get; set; }
+    [Required]
     public string Time { get; set; }  // If you want time separately
+    [Required]
     public string Location { get; set; }  // You can add Google Maps URL here later
     public string Description { get; set; }
     public string ImageUrl { get; set; }  // URL for the image
