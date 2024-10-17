c4f0780e7af10ff8dcded1526118e6e71982b415
NiamhBrowne <niamhb@alchemytechs.com>
2024-10-17 11:16:11 +0100

    Commented out code and changed views

--- FinalProject.Data/Entities/Event.cs
+++ FinalProject.Data/Entities/Event.cs
@@ -8,9 +8,9 @@ public class Event
     [Required]
     public string Title { get; set; }
     [Required]
-    public DateTime Date { get; set; }
-    [Required]
-    public string Time { get; set; }  // time separate to date
+    public DateTime EventTime { get; set; }
+    // [Required]
+    // public string Time { get; set; }  // time separate to date
     [Required]
     public string Location { get; set; }  // I can add Google Maps URL here later
     public string Description { get; set; }
@@ -18,6 +18,6 @@ public class Event
     public string ImageUrl { get; set; }  // URL for the image
     public int UserId { get; set; }  // Foreign key for user who created the event
 
-    public bool IsPast => Date < DateTime.Now;
+    // public bool IsPast => Date < DateTime.Now;
 }
 
--- FinalProject.Web/Controllers/FutureController.cs
+++ FinalProject.Web/Controllers/FutureController.cs
@@ -1,110 +1,110 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using System.Security.Claims;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Authentication.Cookies;
+// using Microsoft.AspNetCore.Authentication;
+// using Microsoft.AspNetCore.Authorization;
+// using System.Security.Claims;
 
-using FinalProject.Data.Entities;
-using FinalProject.Data.Services;
-using FinalProject.Data.Security;
-using FinalProject.Web.Models.User;
+// using FinalProject.Data.Entities;
+// using FinalProject.Data.Services;
+// using FinalProject.Data.Security;
+// using FinalProject.Web.Models.User;
 
-namespace FinalProject.Web.Controllers;
+// namespace FinalProject.Web.Controllers;
 
-public class FutureController : BaseController
-{
-    private readonly IEventService _eventService;
+// public class FutureController : BaseController
+// {
+//     private readonly IEventService _eventService;
 
-    public FutureController(IEventService eventService)
-    {
-        _eventService = eventService;
-    }
+//     public FutureController(IEventService eventService)
+//     {
+//         _eventService = eventService;
+//     }
 
-    // public IActionResult Index(int page = 1, int size = 20, string order = "id", string direction = "asc")
-    // {
-    //     var pagedEvents = _eventService.GetEvents(page, size, order, direction);
-    //     return View(pagedEvents);
-    // }
-
-
-    // GET: List of Future Events
-    public IActionResult Index(int page = 1, int size = 20, string order = "id", string direction = "asc")
-    {
-        var futureEvents = _eventService.GetFutureEvents(page, size, order, direction);
-        return View(futureEvents);
-    }
-
-    // GET: Create New Future Event (Admin Only)
-    [Authorize(Roles = "admin")]
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: Create New Future Event (Admin Only)
-    [Authorize(Roles = "admin")]
-    [HttpPost]
-    public IActionResult Create(Event newEvent)
-    {
-        if (ModelState.IsValid)
-        {
-            _eventService.AddEvent(newEvent);
-            return RedirectToAction("Index");
-        }
-        return View(newEvent);
-    }
-
-    // GET: Edit Event (Admin Only)
-    [Authorize(Roles = "admin")]
-    public IActionResult Edit(int id)
-    {
-        var eventToEdit = _eventService.GetEventById(id);
-        if (eventToEdit == null)
-        {
-            return NotFound();
-        }
-        return View(eventToEdit);
-    }
-
-    // POST: Edit Event (Admin Only)
-    [Authorize(Roles = "admin")]
-    [HttpPost]
-    public IActionResult Edit(Event updatedEvent)
-    {
-        if (ModelState.IsValid)
-        {
-            _eventService.UpdateEvent(updatedEvent);
-            return RedirectToAction("Index");
-        }
-        return View(updatedEvent);
-    }
-
-    // DELETE: Delete Event (Admin Only)
-    [Authorize(Roles = "admin")]
-    [HttpPost]
-    public IActionResult Delete(int id)
-    {
-        var eventToDelete = _eventService.GetEventById(id);
-        if (eventToDelete != null)
-        {
-            _eventService.DeleteEvent(id);
-        }
-        return RedirectToAction("Index");
-    }
-
-    // Method to move expired Future Events to Past Events
-    [HttpPost]
-    public void MovePastEvents()
-    {
-        var futureEvents = _eventService.GetFutureEvents(1, int.MaxValue, "id", "asc");
-        var expiredEvents = futureEvents.Data.Where(e => e.IsPast).ToList();
-
-        foreach (var evt in expiredEvents)
-        {
-            _eventService.MoveEventToPast(evt);  // Move without setting IsPast manually
-        }
-    }
+//     // public IActionResult Index(int page = 1, int size = 20, string order = "id", string direction = "asc")
+//     // {
+//     //     var pagedEvents = _eventService.GetEvents(page, size, order, direction);
+//     //     return View(pagedEvents);
+//     // }
 
 
-}
+//     // GET: List of Future Events
+//     public IActionResult Index(int page = 1, int size = 20, string order = "id", string direction = "asc")
+//     {
+//         var futureEvents = _eventService.GetFutureEvents(page, size, order, direction);
+//         return View(futureEvents);
+//     }
+
+//     // GET: Create New Future Event (Admin Only)
+//     [Authorize(Roles = "admin")]
+//     public IActionResult Create()
+//     {
+//         return View();
+//     }
+
+//     // POST: Create New Future Event (Admin Only)
+//     [Authorize(Roles = "admin")]
+//     [HttpPost]
+//     public IActionResult Create(Event newEvent)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             _eventService.AddEvent(newEvent);
+//             return RedirectToAction("Index");
+//         }
+//         return View(newEvent);
+//     }
+
+//     // GET: Edit Event (Admin Only)
+//     [Authorize(Roles = "admin")]
+//     public IActionResult Edit(int id)
+//     {
+//         var eventToEdit = _eventService.GetEventById(id);
+//         if (eventToEdit == null)
+//         {
+//             return NotFound();
+//         }
+//         return View(eventToEdit);
+//     }
+
+//     // POST: Edit Event (Admin Only)
+//     [Authorize(Roles = "admin")]
+//     [HttpPost]
+//     public IActionResult Edit(Event updatedEvent)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             _eventService.UpdateEvent(updatedEvent);
+//             return RedirectToAction("Index");
+//         }
+//         return View(updatedEvent);
+//     }
+
+//     // DELETE: Delete Event (Admin Only)
+//     [Authorize(Roles = "admin")]
+//     [HttpPost]
+//     public IActionResult Delete(int id)
+//     {
+//         var eventToDelete = _eventService.GetEventById(id);
+//         if (eventToDelete != null)
+//         {
+//             _eventService.DeleteEvent(id);
+//         }
+//         return RedirectToAction("Index");
+//     }
+
+//     // Method to move expired Future Events to Past Events
+//     [HttpPost]
+//     public void MovePastEvents()
+//     {
+//         var futureEvents = _eventService.GetFutureEvents(1, int.MaxValue, "id", "asc");
+//         var expiredEvents = futureEvents.Data.Where(e => e.IsPast).ToList();
+
+//         foreach (var evt in expiredEvents)
+//         {
+//             _eventService.MoveEventToPast(evt);  // Move without setting IsPast manually
+//         }
+//     }
+
+
+// }
 
