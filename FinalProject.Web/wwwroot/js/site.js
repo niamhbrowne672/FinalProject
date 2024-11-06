3a0fcf561f5f6f2d0be59c2d94bcc406cf103d39
NiamhBrowne <niamhb@alchemytechs.com>
2024-11-06 22:07:04 +0000

    added to training and added back to top button on screens

--- FinalProject.Web/wwwroot/js/site.js
+++ FinalProject.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Show the "Back to Top" button when scrolled down, hide it otherwise
+
+// Show the "Back to Top" button after scrolling 200px
+window.onscroll = function () {
+    const button = document.getElementById("back-to-top");
+    if (document.documentElement.scrollTop > 200) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+// Smooth scroll to top when the button is clicked
+document.getElementById("back-to-top").onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
